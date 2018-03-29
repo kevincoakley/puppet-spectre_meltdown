c93bddd6817c7db30d53678739903e3b88f42b95
Kevin Coakley <kcoakley@sdsc.edu>
2018-03-29 14:09:37 -0700

    Created spec tests for the spectre_meltdown class

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,6 +1,38 @@
 require 'spec_helper'
+
 describe 'spectre_meltdown' do
-  context 'with default values for all parameters' do
-    it { should contain_class('spectre_meltdown') }
+  it { is_expected.to compile }
+  it { is_expected.to compile.with_all_deps }
+
+  # Test class defaults
+  it do
+    is_expected.to contain_kernel_parameter('nopti').with(
+      ensure: 'absent'
+    )
+  end
+
+  it do
+    is_expected.to contain_kernel_parameter('spectre_v2').with(
+      ensure: 'absent',
+      value: 'off'
+    )
+  end
+
+  # Test ensure = present
+  context 'with ensure => present' do
+    let(:params) { { 'ensure' => 'present' } }
+
+    it do
+      is_expected.to contain_kernel_parameter('nopti').with(
+        ensure: 'present'
+      )
+    end
+
+    it do
+      is_expected.to contain_kernel_parameter('spectre_v2').with(
+        ensure: 'present',
+        value: 'off'
+      )
+    end
   end
 end
