de8eebe1afb0b3c58cfd803cc98a93dd4136231e
Kevin Coakley <kcoakley@sdsc.edu>
2018-11-26 14:01:13 -0800

    Put fact code on a single line

--- lib/facter/spec_store_bypass.rb
+++ lib/facter/spec_store_bypass.rb
@@ -1,8 +1,7 @@
 Facter.add('spec_store_bypass') do
   setcode do
     if File.exist? '/sys/devices/system/cpu/vulnerabilities/spec_store_bypass'
-      Facter::Core::Execution.execute('cat /sys/devices/system/cpu/vulnerabilities/
-      spec_store_bypass')
+      Facter::Core::Execution.execute('cat /sys/devices/system/cpu/vulnerabilities/spec_store_bypass')
     else
       'Vulnerable'
     end
